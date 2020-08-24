4848b33a9173561adf68b3ef48e74c4d36ce6597
kamooni <66233760+kamooni@users.noreply.github.com>
2020-08-24 09:00:48 +0900

    JS: Home contact button

---
+++ main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// Handle scolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  console.log(target);
+  console.log(link);
+  if (link == null) {
+    return;
+  } else {
+    scrollIntoView(link);
+  }
+});
+
+//Handle scrolling when click on the contack_me btn
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
